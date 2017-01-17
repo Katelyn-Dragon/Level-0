d990135c8f93694d78508a419906cb6993e4fbbf
Katelyn-Dragon <league@WTS10.attlocal.net>
2017-01-16 18:26:40 -0800

    BTS

---
+++ src/WriteAMethod.java
@@ -0,0 +1,21 @@
+
+public class WriteAMethod {
+public static void main(String[] args) {
+	iRock("Me ");
+	int b = Age(11);
+	System.out.println(b);
+	repeatString("Angel", 24);
+}
+static void iRock(String B){
+	System.out.println(B + "rocks");
+}
+static int Age(int age){
+	return 2017 - age;
+}
+static void repeatString(String JHope,int Suga){
+	for (int i = 0; i < Suga; i++) {
+		System.out.println(JHope);
+}
+}
+}
+
