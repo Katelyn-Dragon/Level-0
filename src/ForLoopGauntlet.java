f684a5343312d13f6ee3e6094b2557cdacf8ac4b
The League of Amazing Programmers <league@WTS9.attlocal.net>
2017-05-29 19:55:06 -0700

    0

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -1,8 +1,25 @@
 
 public class ForLoopGauntlet {
+	public static void main(String[] args) {
+		int year = 2005;
+		for (int i = 0; i < 12; i++, year++) {
+
+			System.out.println("in year " + year + " I was " + i + " years old");
+		}
+		//
+		for (int q = 0; q < 3; q++) {
+			for (int w = 0; w < 3; w++) {
+				System.out.println(q + " " + w);
+			}
+
+		}
+		//
+
+	}
 
 	public ForLoopGauntlet() {
 		// TODO Auto-generated constructor stub
+
 	}
 
 }
